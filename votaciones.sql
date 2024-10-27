de433869fc23c6c52bbc53a7f660042fb828290a
Matias Roman <mroman.tdf@gmail.com>
2024-10-27 17:06:23 -0300

    Add database schema for votaciones

---
+++ votaciones.sql
@@ -0,0 +1,95 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE IF NOT EXISTS `administradores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+
+CREATE TABLE IF NOT EXISTS `coordinadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idEspecialidad` int(11) DEFAULT NULL,
+  `usuario_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO `coordinadores` (`id`, `idEspecialidad`, `usuario_id`) VALUES
+	(1, 1, '23123123');
+
+CREATE TABLE IF NOT EXISTS `especialidades` (
+  `idEspecialidad` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`idEspecialidad`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO `especialidades` (`idEspecialidad`, `nombre`) VALUES
+	(1, 'informatico');
+
+CREATE TABLE IF NOT EXISTS `imagenes` (
+  `idProyecto` int(11) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO `imagenes` (`idProyecto`, `url`, `id`) VALUES
+	(1, '/fotos_proyectos/1/671831bda961f.jpg', 1),
+	(1, '/fotos_proyectos/1/671831bdb4de3.jpg', 2);
+
+CREATE TABLE IF NOT EXISTS `proyectos` (
+  `idProyecto` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `idEspecialidad` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idProyecto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO `proyectos` (`idProyecto`, `nombre`, `descripcion`, `idEspecialidad`) VALUES
+	(1, 'lala', 'descrip', 1);
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `username` varchar(50) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `dni` varchar(20) NOT NULL,
+  `tokenUsuario` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO `usuarios` (`username`, `pass`, `nombre`, `apellido`, `dni`, `tokenUsuario`) VALUES
+	('matias', 'matias', 'ffff', 'roman', '23123123', '57d623dbf6cf4422dd7f0be0ac200c2f');
+
+CREATE TABLE IF NOT EXISTS `votantes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO `votantes` (`id`, `email`, `nombre`) VALUES
+	(1, 'mroman.tdf@gmail.com', 'Matias Roman');
+
+CREATE TABLE IF NOT EXISTS `votos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idVotante` int(11) DEFAULT NULL,
+  `idProyecto` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
